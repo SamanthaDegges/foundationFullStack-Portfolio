041d21bc8ad3856bc9e074190c26596812eeb86e
Samantha Degges <samanthadegges@gmail.com>
2015-10-23 17:58:10 -0700

    tumblr api not working yet. broke fastclick

--- project/js/app.js
+++ project/js/app.js
@@ -2,6 +2,24 @@ $(document).foundation();
 
 $(document).ready(function() {
 
+//TUMBLR API CALL
+//   var tumblr = require('node_modules/tumblr.js/lib/tumblr.js');
+//   var client = tumblr.createClient({
+//     consumer_key: TUMBLR_CONSUMER_KEY,
+//     consumer_secret: TUMBLR_CONSUMER_SECRET,
+//     token: TUMBLR_TOKEN,
+//     token_secret: TUMBLR_TOKEN_SECRET
+//   });
+//
+// // Make the request
+// client.userInfo(function (err, data) {
+//   if (err) {
+//     console.log("error", err);
+//   } else {
+//     console.log('works!',data);
+//   }
+// });
+
   $(".myTabs").on("click", function(){
     $(".myTabs").removeClass("active");
     $(this).addClass("active");
--- project/js/routes.js
+++ project/js/routes.js
@@ -1,12 +1,12 @@
 'use strict';
 
-angular.module('app', ['ui.router', 'ng-fastclick'])
+angular.module('app', ['ui.router', //'ng-fastclick'
+])
 
 .config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
   $stateProvider
   .state('portfolio', {url: '/', templateUrl: 'views/index.html'})
   .state('about', {url: '/about', templateUrl: 'views/about.html'})
-
 
 });
